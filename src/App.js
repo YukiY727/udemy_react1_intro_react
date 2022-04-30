67a0b5f7b0f84a352211d2b92e1418d76c032ea6
YukiY727 <83635663+YukiY727@users.noreply.github.com>
2022-04-30 17:55:07 +0900

    porps

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
-      <ColorfullMessage color="blue" message="お元気ですか"/>
-      <p style={contentStyle}>元気です</p>
+      <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
+      <ColorfullMessage color="pink">元気です</ColorfullMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
